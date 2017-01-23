9637df35bfc1d3d0de6aa9b054110e14804b2ebd
Wael Farhan <wael34218@gmail.com>
2017-01-24 01:19:16 +0200

    Encoder complete but with fixed time steps

--- mnist_classification_lstm_new.py
+++ mnist_classification_lstm_new.py
@@ -10,7 +10,7 @@ mnist = input_data.read_data_sets('./data/mnist', one_hot=True)
 # hyperparameters
 lr = 0.001
 epoch = 1
-batch_size = 128
+batch_size = 100
 
 n_inputs = 28   # MNIST data input (img shape: 28*28)
 n_steps = 28    # time steps
@@ -18,15 +18,12 @@ n_hidden_units = 128   # neurons in hidden layer
 n_classes = 10      # MNIST classes (0-9 digits)
 
 # tf Graph input
-x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
-y = tf.placeholder(tf.float32, [None, n_classes])
-
-W1 = tf.Variable(tf.random_normal([n_inputs, n_hidden_units]))
-W2 = tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
-B1 = tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ]))
-B2 = tf.Variable(tf.constant(0.1, shape=[n_classes, ]))
+x = tf.placeholder(tf.float32, [None, n_steps, n_inputs], "Pixels")
+y = tf.placeholder(tf.float32, [None, n_classes], "Classes")
 
 with tf.name_scope("Hidden1") as scope:
+    W1 = tf.Variable(tf.random_normal([n_inputs, n_hidden_units]))
+    B1 = tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ]))
     # transpose the inputs shape from
     # X ==> (128 batch * 28 steps, 28 inputs)
     X = tf.reshape(x, [-1, n_inputs])
@@ -39,9 +36,9 @@ with tf.name_scope("Hidden1") as scope:
 
 with tf.name_scope("LSTM") as scope:
     # basic LSTM Cell.
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
+    # lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
     # lstm cell is divided into two parts (c_state, h_state)
-    init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
+    # init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
 
     # You have 2 options for following step.
     # 1: tf.nn.rnn(cell, inputs);
@@ -51,9 +48,17 @@ with tf.name_scope("LSTM") as scope:
     # In here, we go for option 2.
     # dynamic_rnn receive Tensor (batch, steps, inputs) or (steps, batch, inputs) as X_in.
     # Make sure the time_major is changed accordingly.
-    outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=init_state, time_major=False)
+
+    # outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=init_state,
+    #                                         time_major=False)
+
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units)
+    x_seq = tf.split(0, n_steps, X_in)
+    outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, dtype=tf.float32)
 
 with tf.name_scope("Output") as scope:
+    B2 = tf.Variable(tf.constant(0.1, shape=[n_classes, ]))
+    W2 = tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
     # hidden layer for output as the final results
     #############################################
     # results = tf.matmul(final_state[1], weights['out']) + biases['out']
@@ -61,6 +66,7 @@ with tf.name_scope("Output") as scope:
     # # or
     # unpack to list [(batch, outputs)..] * steps
     outputs = tf.unpack(tf.transpose(outputs, [1, 0, 2]))    # states is the last outputs
+    # outputs[-1] to take the final output
     y_pred = tf.matmul(outputs[-1], W2) + B2
 
 with tf.name_scope("Cost") as scope:
@@ -71,19 +77,35 @@ with tf.name_scope("Optimizer") as scope:
 
 init = tf.global_variables_initializer()
 
+w1 = tf.summary.histogram("WHidden", W1)
+b1 = tf.summary.histogram("BHidden", B1)
+w2 = tf.summary.histogram("WOut", W2)
+b2 = tf.summary.histogram("BOut", B2)
+merged_summary_op = tf.summary.merge_all()
+
 with tf.Session() as sess:
     sess.run(init)
+    summary_writer = tf.summary.FileWriter("./logs", sess.graph)
     for iteration in range(epoch):
         total_batch = int(mnist.train.num_examples/batch_size)
         for _ in range(total_batch):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
             batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
             sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
+            summary_str = sess.run(merged_summary_op, feed_dict={x: batch_xs, y: batch_ys})
+            summary_writer.add_summary(summary_str, iteration*total_batch + 1)
         print(sess.run(cost, feed_dict={x: batch_xs, y: batch_ys}))
 
     print("Tuning Completed!")
 
     predictions = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(predictions, "float"))
-    x_test = mnist.test.images.reshape([-1, n_steps, n_inputs])
-    print("Accuracy: ", accuracy.eval({x: batch_xs, y: batch_ys}))
+
+    print("Last Batch Training Accuracy: ", accuracy.eval({x: batch_xs, y: batch_ys}))
+
+    # Hack to calculate testing accuracy. Wasnt able to make the network consume testing data
+    # all at once, so had to pass it in batches.
+    total_batch = int(mnist.test.num_examples/batch_size)
+    batch_xs = mnist.test.images.reshape([-1, n_steps, n_inputs])
+    test_accuracy = accuracy.eval({x: batch_xs, y: mnist.test.labels})
+    print("Testing Accuracy: ", test_accuracy)

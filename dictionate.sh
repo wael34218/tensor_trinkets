287494e5f8202ac03ed4cfc4d57df8af84b9d86d
Wael Farhan <wael34218@gmail.com>
2017-02-06 07:40:16 +0200

    Dictionate remove empty lines

--- dictionate.sh
+++ dictionate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# TODO: Remove word of empty space
+# TODO: Normalize numbers, dates and other stuff
 cat $1 | sed 's/[[:punct:]]//g' | tr '[:upper:]' '[:lower:]' > $1.clean
-cat $1.clean | tr " " "\n" | sort | uniq -c | sort -nr | awk 'BEGIN{i=5}{printf("%s\t%s\n",i,$2);i+=1}' > tmp
+cat $1.clean | tr " " "\n" | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk 'BEGIN{i=5}{printf("%s\t%s\n",i,$2);i+=1}' > tmp
 echo -e '1\t<eos>\n2\t<pad>\n3\t<unk>\n4\t<go>' | cat - tmp > $1.dic
 
 rm tmp
